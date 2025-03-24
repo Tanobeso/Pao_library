769035785b10712e229ee1411186a50972eba4c9
Giuliano <giuliano.banchieri@studenti.unipd.it>
2025-03-24 11:19:43 +0100

    completati header

--- Src/Art.h
+++ Src/Art.h
@@ -1,1 +1,10 @@
-#pragma once
+Class Art public: PhysicalItem{
+private:
+	string style;
+public:
+	Art(string title, string desc, string aut, string size, string style);
+	~Art();
+
+	string GetStyle();
+	void SetStyle(string style);
+}
--- Src/Movie.h
+++ Src/Movie.h
@@ -1,1 +1,11 @@
-#pragma once
+Class Movie public: Video{
+private:
+	unsigned int length;
+public:
+	Movie(string title, string desc, string publ, string genre, string cast, string dir, unsigned int min);
+	~Movie();
+
+	string GetLength(); 
+	void SetLength(unsigned int h, unsigned int min);
+}
+
